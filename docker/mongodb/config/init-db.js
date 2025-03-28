c93f6c6e54a0266fb721e4d84dbe5e79031894a1
Kilane Dounia <dounia.kilane@student.hepl.be>
2025-03-28 21:53:53 +0100

    Clean version of ajoutDB-dounia-scrum328 without MariaDB volumes

--- docker/mongodb/config/init-db.js
+++ docker/mongodb/config/init-db.js
@@ -1,12 +1,19 @@
 db = connect("mongodb://localhost:27017/admin");
-db.createUser({
-    user: "myuser",
-    pwd: "SparringMASI!",
-    roles: [{ role: "readWrite", db: "admin" }]
-});
-databases = ["db_auth"];
 
-databases.forEach(dbName => {
-    db.getSiblingDB(dbName).createCollection("init");
-    print("Database created: " + dbName);
+// Create the root user in the admin database
+db.createUser({
+    user: "root",
+    pwd: "SparringMASI!",
+    roles: [
+        { role: "userAdminAnyDatabase", db: "admin" },
+        { role: "readWriteAnyDatabase", db: "admin" }
+    ]
 });
+
+// Connect to the auth_db database
+db = connect("mongodb://root:SparringMASI!@localhost:27017/auth_db?authSource=admin");
+db.createCollection("placeholder"); // create a collection to initialize the database
+
+// Connect to the chat_db database
+db = connect("mongodb://root:SparringMASI!@localhost:27017/chat_db?authSource=admin");
+db.createCollection("placeholder");
