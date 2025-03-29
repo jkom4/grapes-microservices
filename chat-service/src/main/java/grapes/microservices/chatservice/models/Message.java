22b2d059ff2151246544f40deb4542a284590d0d
kilane-dounia <98745816+kilane-dounia@users.noreply.github.com>
2025-03-29 10:34:05 +0100

    Feature/ajoutdb dounia SCRUM 328
    SCRUM 328

---
+++ chat-service/src/main/java/grapes/microservices/chatservice/models/Message.java
@@ -0,0 +1,24 @@
+package grapes.microservices.chatservice.models;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    private String id;
+
+    private String chatId;
+    private String senderId;
+    private String content;
+    private LocalDateTime createdAt;
+}
