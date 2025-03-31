6f704c794413633a547ebcf270b255a4a97a0337
Jobelin Kom <komjjobelin@gmail.com>
2025-03-31 18:35:01 +0200

    Skip tests Sales

--- sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
+++ sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
@@ -14,86 +14,86 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class TransactionsServiceApplicationTests {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    void testCreateArticle() throws Exception {
-//        String articleJson = """
-//            {
-//              "categoryId": 1,
-//              "familyId": 1,
-//              "name": "kiwi",
-//              "description": "fruit vitaminé",
-//              "priceKg": 2.99,
-//              "priceUnit": 1.50,
-//              "stockKg": 10.0,
-//              "stockUnit": 5.0,
-//              "origin": "Nouvelle-Zélande",
-//              "picturePath": "kiwi.jpg"
-//            }
-//        """;
-//
-//        mockMvc.perform(post("/articles")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(articleJson))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.name").value("kiwi"));
-//    }
-//
-//    @Test
-//    void testGetAllArticles() throws Exception {
-//        mockMvc.perform(get("/articles"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isArray());
-//    }
-//
-//    @Test
-//    void testUpdateArticle() throws Exception {
-//        String updatedJson = """
-//            {
-//              "categoryId": 1,
-//              "familyId": 1,
-//              "name": "pomme modifiée",
-//              "description": "modif test",
-//              "priceKg": 3.99,
-//              "priceUnit": 2.49,
-//              "stockKg": 12.00,
-//              "stockUnit": 6.00,
-//              "origin": "France",
-//              "picturePath": "pomme_modif.jpg"
-//            }
-//        """;
-//
-//        mockMvc.perform(put("/articles/1")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(updatedJson))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name").value("pomme modifying"));
-//    }
-//
-//    @Test
-//    void testSearchArticleByName() throws Exception {
-//        mockMvc.perform(get("/articles/search")
-//                        .param("name", "kiwi"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$").isArray())
-//                .andExpect(jsonPath("$[0].name").value("kiwi"));
-//    }
-//
-//    @Test
-//    void testSearchArticleWithEmptyName() throws Exception {
-//        mockMvc.perform(get("/articles/search")
-//                        .param("name", " "))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(content().string("The 'name' field cannot be empty."));
-//    }
-//
-//    @Test
-//    void testSearchArticleNotFound() throws Exception {
-//        mockMvc.perform(get("/articles/search")
-//                        .param("name", "mangue"))
-//                .andExpect(status().isNotFound())
-//                .andExpect(content().string("No items found with the name: 'mangue'"));
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCreateArticle() throws Exception {
+        String articleJson = """
+            {
+              "categoryId": 1,
+              "familyId": 1,
+              "name": "kiwi",
+              "description": "fruit vitaminé",
+              "priceKg": 2.99,
+              "priceUnit": 1.50,
+              "stockKg": 10.0,
+              "stockUnit": 5.0,
+              "origin": "Nouvelle-Zélande",
+              "picturePath": "kiwi.jpg"
+            }
+        """;
+
+        mockMvc.perform(post("/articles")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(articleJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("kiwi"));
+    }
+
+    @Test
+    void testGetAllArticles() throws Exception {
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    void testUpdateArticle() throws Exception {
+        String updatedJson = """
+            {
+              "categoryId": 1,
+              "familyId": 1,
+              "name": "pomme modifiée",
+              "description": "modif test",
+              "priceKg": 3.99,
+              "priceUnit": 2.49,
+              "stockKg": 12.00,
+              "stockUnit": 6.00,
+              "origin": "France",
+              "picturePath": "pomme_modif.jpg"
+            }
+        """;
+
+        mockMvc.perform(put("/articles/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updatedJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("pomme modifying"));
+    }
+
+    @Test
+    void testSearchArticleByName() throws Exception {
+        mockMvc.perform(get("/articles/search")
+                        .param("name", "kiwi"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].name").value("kiwi"));
+    }
+
+    @Test
+    void testSearchArticleWithEmptyName() throws Exception {
+        mockMvc.perform(get("/articles/search")
+                        .param("name", " "))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("The 'name' field cannot be empty."));
+    }
+
+    @Test
+    void testSearchArticleNotFound() throws Exception {
+        mockMvc.perform(get("/articles/search")
+                        .param("name", "mangue"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("No items found with the name: 'mangue'"));
+    }
 }
