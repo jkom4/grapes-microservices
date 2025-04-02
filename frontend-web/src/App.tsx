ed61644de0f5882008d71e062f86cccad3d22837
Mathys FRANCO <mathysf41@gmail.com>
2025-04-02 19:27:35 +0200

    Implementation of Typescript Architecture and TailwindCSS

---
+++ frontend-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="text-center text-3xl font-bold text-blue-500">
+            <h1 className="text-3xl font-bold underline">test</h1>
+        </div>
+    );
+};
+
+export default App;
