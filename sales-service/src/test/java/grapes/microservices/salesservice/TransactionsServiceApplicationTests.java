bab3075dff14ebdaa018c9054dc6e4dc83bc5daa
jkom <jkom4dev@gmail.com>
2025-04-03 14:22:04 +0200

    Update TransactionsServiceApplicationTests.java

--- sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
+++ sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
@@ -8,8 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
