feaecf590a7888ecece72bdeb9a3427a2e85c582
Mathys FRANCO <mathysf41@gmail.com>
2025-04-07 13:11:11 +0200

    Fix implementations

---
+++ frontend-web/src/pages/ArticleDetails.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ArticleDetails from "../sections/articleDetails/DetailsSection";
+
+export default function DetailsPage() {
+    return (
+        <>
+            <ArticleDetails />
+        </>
+    );
+}
