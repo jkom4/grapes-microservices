2662c41b109bfd040fc72c4946c61e441e796b3f
Kilane Dounia <dounia.kilane@student.hepl.be>
2025-04-07 23:00:41 +0200

    Setup of Master-Slave Replication for MongoDB

--- docker/mongodb/config/init-db.js
+++ docker/mongodb/config/init-db.js
@@ -1,25 +1,9 @@
-db = connect("mongodb://localhost:27017/admin");
+db = connect("mongodb://root:SparringMASI!@grapes-mongodb-primary:27017/admin?authSource=admin");
 
-// Initialiser le ReplicaSet si ce n'est pas déjà fait
-var status = rs.status();
-if (status.ok === 0) {
-    rs.initiate(
-        {
-            _id: "rs0",
-            members: [
-                { _id: 0, host: "grapes-mongodb-primary:27017" },
-                { _id: 1, host: "grapes-mongodb-secondary:27017" },
-                { _id: 2, host: "grapes-mongodb-arbiter:27017", arbiterOnly: true }
-            ]
-        }
-    );
-    print("✅ ReplicaSet initiated.");
-} else {
-    print("ℹ️ ReplicaSet already initiated.");
-}
-
-db = connect("mongodb://root:SparringMASI!@localhost:27017/auth_db?authSource=admin");
+//Auth db
+db = connect("mongodb://root:SparringMASI!@grapes-mongodb-primary:27017/auth_db?authSource=admin");
 db.createCollection("placeholder");
 
-db = connect("mongodb://root:SparringMASI!@localhost:27017/chat_db?authSource=admin");
+//Chat db
+db = connect("mongodb://root:SparringMASI!@grapes-mongodb-primary:27017/chat_db?authSource=admin");
 db.createCollection("placeholder");
--- docker/mongodb/config/init-replica.js
+++ docker/mongodb/config/init-replica.js
@@ -1,8 +1,31 @@
-rs.initiate({
-    _id: "rs0",
-    members: [
-        { _id: 0, host: "grapes-mongodb-primary:27017" },
-        { _id: 1, host: "grapes-mongodb-secondary:27017" },
-        { _id: 2, host: "grapes-mongodb-arbiter:27017", arbiterOnly: true }
-    ]
-});
+db = connect("mongodb://root:SparringMASI!@grapes-mongodb-primary:27017/admin?authSource=admin");
+
+try {
+    var status = rs.status();
+    if (status.ok === 0) {
+        print("🔄 Initiating ReplicaSet...");
+        rs.initiate({
+            _id: "rs0",
+            members: [
+                { _id: 0, host: "grapes-mongodb-primary:27017" },
+                { _id: 1, host: "grapes-mongodb-secondary:27017" },
+                { _id: 2, host: "grapes-mongodb-arbiter:27017", arbiterOnly: true }
+            ]
+        });
+        print("✅ ReplicaSet initiated.");
+    } else {
+        print("ℹ️ ReplicaSet already initiated.");
+    }
+} catch (e) {
+    print("⚠️ Waiting for Mongo to be ready, retrying in a few seconds...");
+    sleep(5000); //wait 5 secondes
+    rs.initiate({
+        _id: "rs0",
+        members: [
+            { _id: 0, host: "grapes-mongodb-primary:27017" },
+            { _id: 1, host: "grapes-mongodb-secondary:27017" },
+            { _id: 2, host: "grapes-mongodb-arbiter:27017", arbiterOnly: true }
+        ]
+    });
+    print("✅ ReplicaSet initiated after retry.");
+}
