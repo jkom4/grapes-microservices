09adc80911016e90761b10ddab85ec93eee64ce8
NassimBelli <nassim.belli@student.hepl.be>
2025-04-08 13:15:07 +0200

    Creation Queue for rabbitMQ corrected

--- sales-service/src/main/java/grapes/microservices/salesservice/config/RabbitMQConfig.java
+++ sales-service/src/main/java/grapes/microservices/salesservice/config/RabbitMQConfig.java
@@ -8,9 +8,15 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String ORDER_QUEUE = "orderQueue";
+    public static final String ORDER_PAID_QUEUE = "order-paid-queue";
 
     @Bean
     public Queue orderQueue() {
-        return new Queue(ORDER_QUEUE, false);
+        return new Queue(ORDER_QUEUE, true); // true is for create a new Queue
+    }
+
+    @Bean
+    public Queue orderPaidQueue() {
+        return new Queue(ORDER_PAID_QUEUE, true); // true is for create a new queue
     }
 }
