9399ecb8be3703cce3bce3bd79622e740ea3227d
NassimBelli <nassim.belli@student.hepl.be>
2025-04-09 11:44:06 +0200

    fix: connect sales-service to RabbitMQ and auto-create queues

--- sales-service/src/main/java/grapes/microservices/salesservice/controllers/CartController.java
+++ sales-service/src/main/java/grapes/microservices/salesservice/controllers/CartController.java
@@ -20,7 +20,7 @@ import java.io.FileNotFoundException;
  * confirming payment, and retrieving orders.
  */
 @RestController
-@RequestMapping("/cll/cart")
+@RequestMapping("/clm/cart")
 public class CartController {
 
     @Autowired
