157cbca50e13bf3ddc86f14c7911c19879ff83a1
jkom <jkom4dev@gmail.com>
2025-04-10 13:43:55 +0200

    Update TransactionsServiceApplicationTests.java

--- sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
+++ sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
@@ -15,7 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 
 import java.time.LocalDateTime;
 
@@ -274,7 +274,7 @@ class TransactionsServiceApplicationTests {
     }
 
     @Test
-    @Ignore
+    @Disabled("Ce test est ignoré")
     void testEndToEndOrderPaymentAndDeliveryCreation() throws Exception {
 
         // Create Family
