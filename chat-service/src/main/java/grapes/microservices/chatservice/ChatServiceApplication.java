72a71fd924bc2c0842abdc19bcded022da1898bc
Kilane Dounia <dounia.kilane@student.hepl.be>
2025-04-12 00:04:36 +0200

    Backend chat service update

--- chat-service/src/main/java/grapes/microservices/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/grapes/microservices/chatservice/ChatServiceApplication.java
@@ -2,10 +2,8 @@ package grapes.microservices.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "grapes.microservices.chatservice.repositories")
 public class ChatServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatServiceApplication.class, args);
--- chat-service/src/main/java/grapes/microservices/chatservice/security/SecurityConfig.java
+++ chat-service/src/main/java/grapes/microservices/chatservice/security/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private static final String[] SWAGGER_PATHS = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/swagger-ui/**"};
+    private static final String[] SWAGGER_PATHS = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/webjars/swagger-ui/**", "/api/chat/**"};
 
     @Bean
     public OpenAPI customOpenAPI() {
