190505740a131ed713ef5c0e94ce1ae0590d6eef
jkom <jkom4dev@gmail.com>
2025-04-16 09:39:33 +0200

    Delete auth-service/auth-backend/src/main/java/grapes/microservices/authservice/masiid directory
    Delete MASI ID

--- auth-service/auth-backend/src/main/java/grapes/microservices/authservice/masiid/MasiidUserRepository.java
+++
@@ -1,6 +0,0 @@
-package grapes.microservices.authservice.masiid;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MasiidUserRepository extends MongoRepository<MasiIdUser, String> {
-}
