6478b47f25656ea6dab17fe05937986a8994c9ca
Kotiyev Nasser <nasser.kotiyev@student.hepl.be>
2025-04-16 23:17:03 +0200

    SCRUM 164 NASSER
    Frontend of Chat

---
+++ frontend-chat/src/main/java/grapes/microservices/frontendchat/models/services/IGrapesApi.java
@@ -0,0 +1,24 @@
+package grapes.microservices.frontendchat.models.services;
+
+import grapes.microservices.frontendchat.models.Message;
+import grapes.microservices.frontendchat.models.Topic;
+import grapes.microservices.frontendchat.models.User;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface IGrapesApi {
+    /** Fetches the list of all topics */
+    CompletableFuture<List<Topic>> fetchTopics();
+
+    /**
+     * Sends a message through the API (for wider distribution or persistence).
+     */
+    void postMessage(Topic topic, Message message);
+
+    /** Fetches the list of messages sent on a topic. */
+    CompletableFuture<List<Message>> fetchMessages(int topicId);
+
+    /** Authenticate the user with given token, returns user object. */
+    CompletableFuture<User> authUser(String token);
+}
