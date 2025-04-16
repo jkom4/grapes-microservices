5910efd75d67489e279f6c6cfa9a6b78a42f6178
Jobelin Kom <komjjobelin@gmail.com>
2025-04-16 09:53:37 +0200

    resolve test

--- sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
+++ sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(RabbitTestConfig.class)
 @ActiveProfiles(value = "test")
 class TransactionsServiceApplicationTests {
     @Autowired
