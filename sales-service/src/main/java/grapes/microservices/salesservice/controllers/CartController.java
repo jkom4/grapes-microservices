aec308e1f50c0e5a5a1e48e650e291294243e037
Jobelin Kom <komjjobelin@gmail.com>
2025-04-19 10:14:55 +0200

    Ajust for founctionnal

--- sales-service/src/main/java/grapes/microservices/salesservice/controllers/CartController.java
+++ sales-service/src/main/java/grapes/microservices/salesservice/controllers/CartController.java
@@ -20,7 +20,7 @@ import java.io.FileNotFoundException;
  * confirming payment, and retrieving orders.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/clm/cart")
 public class CartController {
 
