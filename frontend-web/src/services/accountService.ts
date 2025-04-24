2572026869b8d8ecbe14b941d58be6ab1ca1f2a0
jkom <jkom4dev@gmail.com>
2025-04-24 16:29:12 +0200

    Feature/scrum 12 jkom

--- frontend-web/src/services/accountService.ts
+++ frontend-web/src/services/accountService.ts
@@ -1,7 +1,7 @@
 import {orderAPI} from "./httpCommon";
 import Order from "../utils/models/Order";
 
-export const fetchOrderHistory = async (userId: number): Promise<Order[]> => {
+ const fetchOrderHistory = async (userId: number): Promise<Order[]> => {
     try {
         const url = `${orderAPI.baseURL}${orderAPI.endpoints.orderHistory(userId)}`;
         const response = await fetch(url, {
--- sales-service/src/main/java/grapes/microservices/salesservice/controllers/OrderController.java
+++ sales-service/src/main/java/grapes/microservices/salesservice/controllers/OrderController.java
@@ -13,11 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/cll/orders")
 public class OrderController {
 
