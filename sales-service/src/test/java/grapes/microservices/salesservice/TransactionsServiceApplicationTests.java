f31e8d1cd05a085ca5b0bfc922d95fe4418afbec
Jobelin Kom <komjjobelin@gmail.com>
2025-04-25 14:49:18 +0200

    correct test

--- sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
+++ sales-service/src/test/java/grapes/microservices/salesservice/TransactionsServiceApplicationTests.java
@@ -106,7 +106,7 @@ class TransactionsServiceApplicationTests {
 
         Delivery delivery = Delivery.builder()
                 .orderId(8)
-                .userId(1)
+                .userId("1")
                 .deliveryStatusId(deliveryStatusRepository.findByLabel("Pending").get().getId())
                 .deliveryDate(LocalDateTime.now())
                 .build();
@@ -400,7 +400,7 @@ class TransactionsServiceApplicationTests {
         // MANUALLY simulate delivery creation (simulate RabbitMQ behavior)
         Delivery delivery = Delivery.builder()
                 .orderId(orderId)
-                .userId(2) // 🛠️ Choose an existing deliveryMan ID
+                .userId("2") // 🛠️ Choose an existing deliveryMan ID
                 .deliveryStatusId(1) // pending
                 .deliveryDate(LocalDateTime.now())
                 .build();
