9fdc986c598a14f36365167d56930cf54d8f0be0
Mathys FRANCO <mathysf41@gmail.com>
2025-04-26 16:21:37 +0200

    Implement little CLL fix

--- mobile-cll/app/src/main/java/com/example/mobile_cll/views/components/tripDetails/MyTripCardDetails.kt
+++ mobile-cll/app/src/main/java/com/example/mobile_cll/views/components/tripDetails/MyTripCardDetails.kt
@@ -53,7 +53,6 @@ fun TripInfoCard(
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(text = trip.name, fontWeight = FontWeight.Bold, fontSize = 16.sp)
-                    Text(text = "Dist: ${trip.distance}", fontSize = 12.sp, color = MaterialTheme.colorScheme.tertiary)
                 }
                 Spacer(modifier = Modifier.height(4.dp))
 
